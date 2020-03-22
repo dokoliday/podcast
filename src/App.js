0d033a3394bcf40d2c9425f6debf01966fd2aaf4
dokoliday <smirnoff.awa@gmail.com>
2020-03-22 08:54:15 +0100

    ADD : React-boostrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import DisplayPodcast from "./containers/DisplayPodcast";
-import {audioFiles} from "../src/data/podcastFiles";
+import { audioFiles } from "../src/data/podcastFiles";
 
 const App = () => {
   return (
