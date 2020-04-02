a85f58d7c1ee34d6e6c1c3a7a46091067522abdc
dokoliday <smirnoff.awa@gmail.com>
2020-04-02 22:59:59 +0200

    test probleme heroku

--- src/data/podcastFiles.js
+++ src/data/podcastFiles.js
@@ -1,18 +1,10 @@
 export const audioFiles = [
   {
-    src: "https://archive.org/download/BioHazardRadio/Jour%201%20molles.mp3",
+    src: "https://archive.org/download/BioHazardRadio/PILOTE.mp3",
     title: "first day confinement ",
     img: "https://archive.org/download/BioHazardRadio/femme.jpg",
     resume: "this is the resume for the first day confinement etc ....",
     lat: 48,
     long: 33
-  },
-  {
-    src: "https://archive.org/download/BioHazardRadio/Jour%202.mp3",
-    title: "second day confinement ",
-    img: "https://archive.org/download/BioHazardRadio/point.jpeg",
-    resume: "this is the resume for the second day confinement etc ....",
-    lat: 43,
-    long: 32
   }
 ];
