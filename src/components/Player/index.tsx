167316e3218f98a5c0954f395400563ef896809a
dokoliday <smirnoff.awa@gmail.com>
2020-04-06 12:10:55 +0200

    Fix : unused code

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,23 +2,6 @@ import React from "react";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import "./player.css";
-import play from "../../assets/play.png";
-import pause from "../../assets/pause.png";
-
-const rearrangedPlayer = [
-  {
-    className: "top",
-    innerComponents: [
-      {
-        type: "play",
-      },
-      { type: "time" },
-      {
-        type: "seek",
-      }
-    ]
-  }
-];
 
 interface Props {
   src: string;
